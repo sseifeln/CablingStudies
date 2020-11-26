463a5af6ed3e3b555d9feed353747906624cef90
Sarah Seif El Nasr <sarah.storey@cern.ch>
2020-11-26 15:46:05 +0100

    Can now see module count, and min/max fluence

--- QuickLoad.py
+++ QuickLoad.py
@@ -52,12 +52,18 @@ def mergeMaps( pLayoutId = referenceLayout , modType = "PS10G" ) :
 	cMerged = pd.merge(cCablingMap, cRadMap, how='inner', on="Module DetId")
 	df =  cMerged.loc[cMerged['DTC name'].str.contains(modType)].loc[:, ['DTC CMSSW Id', 'DTC name', ' sensorsFluenceMean_Hb',' sensorsFluenceMax_Hb']]
 	# min fluence (mean and max)
-	df = df.groupby('DTC CMSSW Id').min().add_suffix('min').reset_index()
+	df_min  = df.groupby('DTC CMSSW Id').min().add_suffix('min').reset_index()
+	df_max  = df.groupby('DTC CMSSW Id').max().add_suffix('max').reset_index()
+	df_mean = df.groupby('DTC CMSSW Id').mean().add_suffix('max').reset_index()
 	# module count 
 	df1 = cMerged.loc[:, ['DTC CMSSW Id']]
 	df1['Module Count'] = np.zeros(len(cMerged))
 	df1 = df1.loc[ cMerged['DTC name'].str.contains(modType)].groupby(["DTC CMSSW Id"]).count().sort_values(by=['DTC CMSSW Id'])
-	return pd.merge(df1, df, how='inner', on="DTC CMSSW Id")
+	# then merge with module count 
+	df_m1 = pd.merge(df1, df_min, how='inner', on="DTC CMSSW Id")
+	df_m2 = pd.merge(df_m1, df_max, how='inner', on="DTC CMSSW Id")
+	return pd.merge(df_mean, df_max, how='inner', on="DTC CMSSW Id")
+
 	
 
 
